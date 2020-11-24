134fcec42ee8ac8eabf0f9ebae47144b246b9286
MikeTheFyke <mikefyke@hotmail.com>
2020-11-23 19:59:42 -0500

    Connecting to index

--- client/express_server.js
+++ client/express_server.js
@@ -6,27 +6,9 @@ app.set("view engine", "ejs")
 app.use('/public', express.static('public'));
 
 /// Index (Home) Route
-app.get("/index", (req, res) => {
-    let templateVars = { urls : urlDatabase };
-    res.render("index", templateVars);
+app.get("/", (req, res) => {
+    res.render("index");
 });
-
-
-/// Pong Canvas Route
-app.get("/pong", (req, res) => {
-    res.render("pong");
-});
-
-/// Side Scroll Canvas Route
-app.get("/sidescroll", (req, res) => {
-    res.render("sidescroll");
-});
-
-/// Side Scroll Canvas Route
-app.get("/froggerP", (req, res) => {
-    res.render("froggerP");
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
