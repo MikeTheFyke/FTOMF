05c2d7093e91552ce270af05b87f9596f719b006
MikeTheFyke <mikefyke@hotmail.com>
2020-11-23 20:27:55 -0500

    Randomized header element y cord

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -1,1 +1,8 @@
-TweenMax.to('#header-container', 1, {opacity: 1, y: "200px", delay: 2}) ;
+
+var minimum = 1;
+var maximum = window.innerHeight;
+console.log(maximum)
+
+var randomNumberGenerator = [...Array(10)].map(() => (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum)-50);
+
+TweenMax.to('#header-container', 1, {opacity: 1, y: randomNumberGenerator, delay: 2}) ;
