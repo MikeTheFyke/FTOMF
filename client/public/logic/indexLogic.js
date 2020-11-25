a7bab2c9860b21dc24404a8f7f6c7cb4e130fc89
MikeTheFyke <mikefyke@hotmail.com>
2020-11-24 19:46:13 -0500

    Added ease to header div bounce

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -2,11 +2,11 @@
 // var minimum = window.innerHeight -150;
 // var maximum = window.innerHeight;
 
-var minimum = 1;
-var maximum = 3;
+var minimum = 0.1;
+var maximum = 2;
 
 // var randomNumberGenerator = [...Array(10)].map(() => (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum));
 var randomNumberGenerator = (Math.random() * (maximum - minimum + 1)) + minimum;
 console.log(randomNumberGenerator)
 
-TweenMax.to('#header-container', randomNumberGenerator, {opacity: 1, y: window.innerHeight - 100, delay: 1}) ;
+TweenMax.to('#header-container', 2.5, {opacity: 1, y: window.innerHeight - 100, delay: 1, ease: Bounce.easeOut }) ;
