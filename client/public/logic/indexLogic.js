ed4b6f8fd77e0610508917885498e652e7ce3ba2
MikeTheFyke <mikefyke@hotmail.com>
2020-11-24 20:00:56 -0500

    Started foot shadow gsap

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -9,4 +9,11 @@
 // var randomNumberGenerator = (Math.random() * (maximum - minimum + 1)) + minimum;
 // console.log(randomNumberGenerator)
 
-TweenMax.to('#header-container', 2.5, {opacity: 1, y: window.innerHeight - 100, delay: 1, ease: Bounce.easeOut }) ;
+TweenMax.to('#header-container', 2.5, { y: window.innerHeight - 100, delay: 1, ease: Bounce.easeOut }) ;
+TweenMax.to('#foot-shadow', 0, {scaleY: 0, scaleX: 0})
+TweenMax.to('#foot-shadow', 1, {scaleY: 0.5, scaleX: 0.75, delay: 0.5})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 1.5})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, scaleX: 0.75, delay: 2})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 2.5})
+// TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, delay: 1.5})
+// TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, delay: 2.25})
