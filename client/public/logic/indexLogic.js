9cae50dcaacc43ec8f896c14f334d58e13afd0a1
MikeTheFyke <mikefyke@hotmail.com>
2020-11-24 20:02:46 -0500

    Adjusting shadow timline

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -11,9 +11,9 @@
 
 TweenMax.to('#header-container', 2.5, { y: window.innerHeight - 100, delay: 1, ease: Bounce.easeOut }) ;
 TweenMax.to('#foot-shadow', 0, {scaleY: 0, scaleX: 0})
-TweenMax.to('#foot-shadow', 1, {scaleY: 0.5, scaleX: 0.75, delay: 0.5})
-TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 1.5})
-TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, scaleX: 0.75, delay: 2})
-TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 2.5})
+TweenMax.to('#foot-shadow', 1, {scaleY: 0.5, scaleX: 0.75, delay: 0.75})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 1.75})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.25, scaleX: 0.25, delay: 2.25})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 2.75})
 // TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, delay: 1.5})
 // TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, delay: 2.25})
