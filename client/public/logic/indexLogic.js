6dfcca9ef3edf174ef4fa5db6f8592817a179711
MikeTheFyke <mikefyke@hotmail.com>
2020-11-25 19:56:43 -0500

    Adjusted shadow gsap

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -12,10 +12,11 @@
 TweenMax.to('#header-container', 2.5, { y: window.innerHeight - 100, delay: 1, ease: Bounce.easeOut }) ;
 
 TweenMax.to('#foot-shadow', 0, {scaleY: 0, scaleX: 0})
-TweenMax.to('#foot-shadow', 1.25, {scaleY: 0.5, scaleX: 1, delay: 1})
-TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 1.75})
-// TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.25, scaleX: 0.25, delay: 2.25})
-// TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, scaleX: 1, delay: 2.75})
+TweenMax.to('#foot-shadow', 1.05, {scaleY: 0.5, scaleX: 1, delay: 1})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.25, scaleX: 0.75, delay: 2.05})
+TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, scaleX: 1, delay: 2.55})
+TweenMax.to('#foot-shadow', 0.25, {scaleY: 0.25, scaleX: 0.75, delay: 3.05})
+TweenMax.to('#foot-shadow', 0.25, {scaleY: 0.5, scaleX: 1, delay: 3.30})
 
 // TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.75, delay: 1.5})
 // TweenMax.to('#foot-shadow', 0.5, {scaleY: 0.5, delay: 2.25})
