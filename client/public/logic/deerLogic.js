571b0eecf1aa155c607426531d6814da2a0b9aa6
MikeTheFyke <mikefyke@hotmail.com>
2020-11-29 19:46:54 -0500

    Split forest element into 4 elements

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -1,3 +1,8 @@
 TweenMax.to('#mountain-image1', 0, {rotation: 45})
 TweenMax.to('#mountain-image2', 0, {rotation: 45})
 TweenMax.to('#mountain-image3', 0, {rotation: 45})
+
+TweenMax.to('#forest-image1', 0, {rotation: 45})
+TweenMax.to('#forest-image2', 0, {rotation: 45})
+TweenMax.to('#forest-image3', 0, {rotation: 45})
+TweenMax.to('#forest-image4', 0, {rotation: 45})
