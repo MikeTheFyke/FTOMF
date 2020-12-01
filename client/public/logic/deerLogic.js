9d1b9837b1ada5e8bbbe7ab4ecabe80f77da2994
MikeTheFyke <mikefyke@hotmail.com>
2020-11-30 19:29:58 -0500

    Cleaned up mountain and forest gsap

--- client/public/logic/deerLogic.js
+++
@@ -1,8 +0,0 @@
-TweenMax.to('#mountain-image1', 0, {rotation: 45})
-TweenMax.to('#mountain-image2', 0, {rotation: 45})
-TweenMax.to('#mountain-image3', 0, {rotation: 45})
-
-TweenMax.to('#forest-image1', 0, {rotation: 45})
-TweenMax.to('#forest-image2', 0, {rotation: 45})
-TweenMax.to('#forest-image3', 0, {rotation: 45})
-TweenMax.to('#forest-image4', 0, {rotation: 45})
