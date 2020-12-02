4c22bd360dfb589a6bf8243862168d6ea770208d
MikeTheFyke <mikefyke@hotmail.com>
2020-12-01 20:00:17 -0500

    Home link text now slides in

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -1,3 +1,6 @@
+TweenMax.to('#home-link', 0, { x: "-100vw" })
+TweenMax.to('#home-link', 1, { x: 0, delay: 1 })
+
 document.getElementById('home-link').addEventListener('mouseover', function(){
     TweenMax.to('#home-link', 1, { scale: 1.25, x: "10%", ease: Elastic.easeOut.config(1, 0.3) })
     console.log("MouseOver")
