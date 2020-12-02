a7235ec81faa55a01b0f00d50ea79b9032668a94
MikeTheFyke <mikefyke@hotmail.com>
2020-12-01 20:06:28 -0500

    Mountain now fades on load and adjusted home link load timing

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -1,5 +1,7 @@
 TweenMax.to('#home-link', 0, { x: "-100vw" })
-TweenMax.to('#home-link', 1, { x: 0, delay: 1 })
+TweenMax.to('#home-link', 2, { x: 0, delay: 1 })
+
+TweenMax.to('#mountain-image', 4, { opacity: 1, delay: 1 })
 
 document.getElementById('home-link').addEventListener('mouseover', function(){
     TweenMax.to('#home-link', 1, { scale: 1.25, x: "10%", ease: Elastic.easeOut.config(1, 0.3) })
