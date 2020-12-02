8cd757b36d8a64dacfcd3c1197c8a2d1feb8036d
MikeTheFyke <mikefyke@hotmail.com>
2020-12-01 19:51:29 -0500

    adjusted home link x position upon hover and mouseout

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -1,8 +1,8 @@
 document.getElementById('home-link').addEventListener('mouseover', function(){
-    TweenMax.to('#home-link', 1, { scale: 2, ease: Elastic.easeOut.config(1, 0.3) })
+    TweenMax.to('#home-link', 1, { scale: 2, x: 150, ease: Elastic.easeOut.config(1, 0.3) })
     console.log("MouseOver")
 });
 
 document.getElementById('home-link').addEventListener('mouseout', function(){
-    TweenMax.to('#home-link', 1, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) })
+    TweenMax.to('#home-link', 1, { scale: 1, x: 0, ease: Elastic.easeOut.config(1, 0.3) })
 });
