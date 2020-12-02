3bc67bd56e1bdb02e03c7a8bc0c1debe0b0495bb
MikeTheFyke <mikefyke@hotmail.com>
2020-12-01 19:38:53 -0500

    Started gsap deerhunt logic

---
+++ client/public/logic/deerLogic.js
@@ -0,0 +1,7 @@
+document.getElementById('home-link').addEventListener('mouseover', function(){
+    TweenMax.to('#home-link', 0, { scaleY: 2})
+});
+
+document.getElementById('home-link').addEventListener('mouseover', function(){
+    TweenMax.to('#home-link', 0, { scaleY: 1})
+});
