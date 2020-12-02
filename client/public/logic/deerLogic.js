47352002d5ac5d3bd99de823bd1b4d7ca64d8798
MikeTheFyke <mikefyke@hotmail.com>
2020-12-01 19:46:44 -0500

    Home link is gsapping

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -1,7 +1,8 @@
 document.getElementById('home-link').addEventListener('mouseover', function(){
-    TweenMax.to('#home-link', 0, { scaleY: 2})
+    TweenMax.to('#home-link', 1, { scale: 2, ease: Elastic.easeOut.config(1, 0.3) })
+    console.log("MouseOver")
 });
 
-document.getElementById('home-link').addEventListener('mouseover', function(){
-    TweenMax.to('#home-link', 0, { scaleY: 1})
+document.getElementById('home-link').addEventListener('mouseout', function(){
+    TweenMax.to('#home-link', 1, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) })
 });
