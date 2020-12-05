ae66047c306c0e763443cafcf2c8556866cc2ccd
MikeTheFyke <mikefyke@hotmail.com>
2020-12-04 22:05:11 -0500

    Adjsuted deer sensor timing

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -2,8 +2,8 @@ TweenMax.to('#home-link', 0, { x: "-100vw" })
 TweenMax.to('#home-link', 2, { x: 0, delay: 1 })
 TweenMax.to('#deer-image', 0, { scaleY: 0})
 
-TweenMax.to('#deer-sensor-outer', 1, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
-TweenMax.to('#deer-sensor', 1, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
+TweenMax.to('#deer-sensor-outer', 2, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
+TweenMax.to('#deer-sensor', 2, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
 
 TweenMax.to('#mountain-image', 4, { opacity: 1, delay: 1 })
 
@@ -18,6 +18,6 @@ document.getElementById('home-link').addEventListener('mouseout', function(){
 
 document.getElementById('deer-sensor').addEventListener('mouseover', function(){
     TweenMax.to('#deer-image', 1, { scaleY: 1, transformOrigin: "bottom center" })
-    TweenMax.to('#deer-sensor-outer', 1, { opacity: 0, zIndex: 0 })
-    TweenMax.to('#deer-sensor', 1, { opacity: 0, zIndex: 0 })
+    TweenMax.to('#deer-sensor-outer', 0.5, { opacity: 0, zIndex: 0 })
+    TweenMax.to('#deer-sensor', 0.5, { opacity: 0, zIndex: 0 })
 });
