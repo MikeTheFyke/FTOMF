c2de49e87158ff5e952f7480c637cf193e3cecde
MikeTheFyke <mikefyke@hotmail.com>
2020-12-04 22:04:18 -0500

    Added gsap hover and deer reveal gsap

--- client/public/logic/deerLogic.js
+++ client/public/logic/deerLogic.js
@@ -2,6 +2,7 @@ TweenMax.to('#home-link', 0, { x: "-100vw" })
 TweenMax.to('#home-link', 2, { x: 0, delay: 1 })
 TweenMax.to('#deer-image', 0, { scaleY: 0})
 
+TweenMax.to('#deer-sensor-outer', 1, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
 TweenMax.to('#deer-sensor', 1, { scale: 1, yoyo: 1, repeat: -1, transformOrigin: "center" })
 
 TweenMax.to('#mountain-image', 4, { opacity: 1, delay: 1 })
@@ -15,6 +16,8 @@ document.getElementById('home-link').addEventListener('mouseout', function(){
     TweenMax.to('#home-link', 1, { scale: 1, x: 0, ease: Elastic.easeOut.config(1, 0.3) })
 });
 
-document.getElementById('grass02').addEventListener('mouseover', function(){
+document.getElementById('deer-sensor').addEventListener('mouseover', function(){
     TweenMax.to('#deer-image', 1, { scaleY: 1, transformOrigin: "bottom center" })
+    TweenMax.to('#deer-sensor-outer', 1, { opacity: 0, zIndex: 0 })
+    TweenMax.to('#deer-sensor', 1, { opacity: 0, zIndex: 0 })
 });
