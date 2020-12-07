2ee1695d007546b9224d37f80a766e6c6bcad3dc
MikeTheFyke <mikefyke@hotmail.com>
2020-12-06 20:09:33 -0500

    Started moon gsap

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -1,1 +1,3 @@
 TweenMax.to("#earth-map", 5, { x: "500px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
+
+TweenMax.to("#terra-beta", 5, { x: "190px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
