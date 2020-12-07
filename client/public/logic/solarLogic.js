6250b0d4d1b69bab069e1b36470def645b314443
MikeTheFyke <mikefyke@hotmail.com>
2020-12-06 20:21:25 -0500

    Added motionplus to gsap logic

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -1,3 +1,14 @@
+
 TweenMax.to("#earth-map", 5, { x: "500px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
 
-TweenMax.to("#terra-beta", 5, { x: "190px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
+// TweenMax.to("#terra-beta", 5, { x: "190px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
+
+
+TweenMax.to("#terra-beta", {
+    motionPath: {
+        path:[ { x: 95, y: 5},
+               { x: 190, y:0 },
+             ],
+curviness: 1
+}, 
+duration: 5, delay: 1, repeat: -1, ease: "Linear.easeInOut" })
