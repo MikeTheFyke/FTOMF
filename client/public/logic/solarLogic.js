155604999b99093e78ef9431963367499952b346
MikeTheFyke <mikefyke@hotmail.com>
2020-12-06 20:36:49 -0500

    Started terra-beta 2 gsap

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -6,8 +6,21 @@ TweenMax.to("#earth-map", 5, { x: "500px", delay: 1, repeat: -1, ease: "Linear.e
 
 TweenMax.to("#terra-beta", {
     motionPath: {
-        path:[ { x: 95, y: 5},
+        path:[ { x: 95, y: 5 },
                { x: 190, y:0 },
+               { x: 95, y:5 },
+               { x: 0, y:5 },
+             ],
+curviness: 1
+}, 
+duration: 5, delay: 1, repeat: -1, ease: "Linear.easeInOut", opacity: 0 })
+
+TweenMax.to("#terra-beta2", {
+    motionPath: {
+        path:[ { x: 95, y: 5 },
+               { x: 190, y:0 },
+               { x: 95, y:5 },
+               { x: 0, y:5 },
              ],
 curviness: 1
 }, 
