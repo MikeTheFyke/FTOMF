f1629a4fc9db0695eeb6b8dbea3a6c3ef47b5253
MikeTheFyke <mikefyke@hotmail.com>
2020-12-07 20:41:12 -0500

    Refactued tweenmax to gsap

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -1,10 +1,9 @@
 
-TweenMax.to("#earth-map", 5, { x: "500px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
-
-// TweenMax.to("#terra-beta", 5, { x: "190px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
 
 
-TweenMax.to("#terra-beta", {
+gsap.to("#earth-map", 5, { x: "500px", delay: 1, repeat: -1, ease: "Linear.easeInOut" })
+
+gsap.to("#terra-beta", {
     motionPath: {
         path:[ { x: 95, y: 5 },
                { x: 190, y:0 },
@@ -15,7 +14,7 @@ curviness: 1
 }, 
 duration: 5, delay: 1, repeat: -1, ease: "Linear.easeInOut", opacity: 0 })
 
-TweenMax.to("#terra-beta2", {
+gsap.to("#terra-beta2", {
     motionPath: {
         path:[ { x: 95, y: 5 },
                { x: 190, y:0 },
