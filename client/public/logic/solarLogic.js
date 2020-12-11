8f904b448b3d2ca16c381c3057854a40a4e46ca2
MikeTheFyke <mikefyke@hotmail.com>
2020-12-10 19:29:27 -0500

    Adjusting venus starting position

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -1,10 +1,10 @@
 var terraStatus = false;
 
 gsap.to("#sun-container", 0, { y: "-100px", x:"45vw", scale: 0.20 })
-gsap.to("#mercury-container", 0, { y: "-300px", x:"35vw", scale: 0.20 })
-gsap.to("#venus-container", 0, { y: "-500px", x:"100px", scale: 0.20 })
+gsap.to("#mercury-container", 0, { y: "-300px", x:"36vw", scale: 0.20 })
+gsap.to("#venus-container", 0, { y: "-500px", x:"26vw", scale: 0.20 })
 
-gsap.to("#terra-container", 0, { y: "-455px", scale: 0.20 })
+gsap.to("#terra-container", 0, { y: "-455px", x:"16vw",scale: 0.20 })
 
 function terraEnlarge(){
     if ( terraStatus = false ){
