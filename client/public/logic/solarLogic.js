374e9709ea647248e9859f7d056108a2ad1b7892
MikeTheFyke <mikefyke@hotmail.com>
2020-12-12 20:08:50 -0500

    Terra reset is functioning

--- client/public/logic/solarLogic.js
+++ client/public/logic/solarLogic.js
@@ -14,55 +14,45 @@ gsap.to("#pluto-container", 0, { y: "-1535px", x:"-44vw", scale: 0.20 })
 function terraEnlarge(){
     if ( terraStatus === false ){
         terraStatus = true;
-        console.log(terraStatus)
+
+        gsap.to("#terra-container", 2, { y: 0, scale: 1 })
+
+        var earthMap = new TimelineMax();
+        earthMap.to("#earth-map", 5, { x: "500px", repeat: -1, ease: "Linear.easeInOut" });
+
+        var terraBeta = new TimelineMax();
+        terraBeta.to("#terra-beta", {
+        motionPath: {
+            path:[ { x: 95, y: 5 },
+                { x: 190, y:0 },
+                { x: 95, y:5 },
+                { x: 0, y:0 },
+                ],
+        curviness: 1
+        }, 
+        duration: 5, ease: "Linear.easeInOut", repeat: -1, opacity: 0 });
+
+        var terraBeta2 = new TimelineMax();
+        terraBeta2.to("#terra-beta2", {
+        motionPath: {
+            path:[ { x: 95, y: 5 },
+                { x: 190, y:0 },
+                { x: 95, y:5 },
+                { x: 0, y:0 },
+                ],
+        curviness: 1
+        }, 
+        duration: 5, repeat: -1, ease: "Linear.easeInOut" });
+        
+        var master = new TimelineMax();
+        master.add(earthMap, terraBeta, terraBeta2)
+            // .add(terraBeta)
+            // .add(terraBeta2);
+            console.log(terraStatus)
     }
     else if ( terraStatus === true ){
         terraStatus = false;
-        console.log(terraStatus)
-    }
-
-    gsap.to("#terra-container", 2, { y: 0, scale: 1 })
-
-    var earthMap = new TimelineMax();
-    earthMap.to("#earth-map", 5, { x: "500px", repeat: -1, ease: "Linear.easeInOut" });
-
-    var terraBeta = new TimelineMax();
-    terraBeta.to("#terra-beta", {
-    motionPath: {
-        path:[ { x: 95, y: 5 },
-               { x: 190, y:0 },
-               { x: 95, y:5 },
-               { x: 0, y:0 },
-             ],
-    curviness: 1
-    }, 
-    duration: 5, ease: "Linear.easeInOut", repeat: -1, opacity: 0 });
-
-    var terraBeta2 = new TimelineMax();
-    terraBeta2.to("#terra-beta2", {
-    motionPath: {
-        path:[ { x: 95, y: 5 },
-               { x: 190, y:0 },
-               { x: 95, y:5 },
-               { x: 0, y:0 },
-             ],
-    curviness: 1
-    }, 
-    duration: 5, repeat: -1, ease: "Linear.easeInOut" });
-    
-    var master = new TimelineMax();
-    master.add(earthMap, terraBeta, terraBeta2)
-        // .add(terraBeta)
-        // .add(terraBeta2);
-}
-
-function buttonTest (){
-    if ( terraStatus === false ){
-        terraStatus = true;
-        console.log(terraStatus)
-    }
-    else if ( terraStatus === true ){
-        terraStatus = false;
+        gsap.to("#terra-container", 0, { y: "-455px", x:"15vw",scale: 0.20 })
         console.log(terraStatus)
     }
 }
