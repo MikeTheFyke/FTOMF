655d2e51f31428c92cc7ecc1f749ba146a65e431
MikeTheFyke <mikefyke@hotmail.com>
2020-12-16 21:20:40 -0500

    Added sun enlarge and reset gsap function

--- client/public/logic/tunnelLogic.js
+++ client/public/logic/tunnelLogic.js
@@ -1,1 +1,13 @@
+var sunStatus = false;
+
 gsap.to("#sun-map", 5, { x: "140px", repeat: -1, ease: "Linear.easeInOut" });
+
+document.getElementById('sun-container').addEventListener('click', function () {
+    if ( sunStatus === false ){
+        sunStatus = true;
+        gsap.to("#sun-container", 2, { x:"-40vw" ,y: "200px", scale: 2 })
+    } else if ( sunStatus === true ){
+        sunStatus = false;
+        gsap.to("#sun-container", 2, { x: 0 ,y: 0, scale: 1 })
+    }
+})
