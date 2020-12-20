625f4688fb82d7c9198b212aee44a73db5433680
MikeTheFyke <mikefyke@hotmail.com>
2020-12-19 21:28:55 -0500

    Grabbing live earth to mars data

--- client/express_server.js
+++ client/express_server.js
@@ -7,6 +7,17 @@ const cheerio = require('cheerio');
 app.set("view engine", "ejs")
 app.use('/public', express.static('public'));
 
+
+request('https://theskylive.com/mars-tracker', (error, response, html) => {
+  if (!error && response.statusCode == 200){
+    const $ = cheerio.load(html)
+
+    const earthMars = $('#disearth');
+    const earthMarsDistance = earthMars.text();
+    console.log("Grabbed : " + earthMarsDistance);
+  }
+});
+
 /// Index (Home) Route
 app.get("/", (req, res) => {
     res.render("index");
@@ -19,15 +30,15 @@ app.get("/deerhunt", (req, res) => {
 
 /// Tunnel Route
 app.get("/tunnel", (req, res) => {
-  request('https://theskylive.com/how-far-is-mars', (error, response, html) => {
-    if (!error && response.statusCode == 200){
-      const $ = cheerio.load(html)
+  // request('https://theskylive.com/how-far-is-mars', (error, response, html) => {
+  //   if (!error && response.statusCode == 200){
+  //     const $ = cheerio.load(html)
 
-      const earthMars = $('.keyinfobox');
-      const earthMarsDistance = earthMars.find('ar').text();
-      console.log(earthMarsDistance);
-    }
-  });
+  //     const earthMars = $('.keyinfobox');
+  //     const earthMarsDistance = earthMars.find('ar').text();
+  //     console.log(earthMarsDistance);
+  //   }
+  // });
   res.render("tunnel");
 });
 
@@ -41,8 +52,8 @@ app.listen(PORT, () => {
 });
 
 
-request('https://theskylive.com/how-far-is-mars', (error, response, html) => {
-  if (!error && response.statusCode == 200){
-    console.log(html)
-  }
-});
+// request('https://theskylive.com/how-far-is-mars', (error, response, html) => {
+//   if (!error && response.statusCode == 200){
+//     console.log(html)
+//   }
+// });
