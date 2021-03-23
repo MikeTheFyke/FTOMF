8511bc91d38ae1eede7efbb3064abb2e8bb90488
MikeTheFyke <mikefyke@hotmail.com>
2021-03-23 11:53:15 -0400

    Adding set fly position gsap logic

--- client/public/logic/flyLogic.js
+++ client/public/logic/flyLogic.js
@@ -16,21 +16,25 @@ createElement = () => {
         randomXValue = Math.floor(Math.random() * (windowWidth - 10)) + 10;
         randomX = randomXValue + "px";
         var newFly = new Image (50,50);
-        newFly.setAttribute("img", "fly"[i]);
+        newFly.setAttribute("id", "fly"+ [i]);
         newFly.src = '/public/images/fly.png';
         newFly.style.position = 'absolute';
         newFly.style.left = randomX;
         newFly.style.top = randomY;
         document.getElementById("flyViewContainer").appendChild(newFly);
-
-        flys.push({
-            id: i,
-            x: randomX,
-            y: randomY
-        })
-        
+            flys.push({
+                id: i,
+                x: randomXValue,
+                y: randomYValue
+            })
         randomY = "";
     }
+    console.log(flys)
+    setElement()
+}
+
+setElement = () => {
+    TweenMax.to('#fly1', 0, { x: "-100vw" })    
 }
 
 TweenMax.to('#flyImage', 0, { x: "-100vw" })
