77e99da178c50f7f02f10c4c8f58b498b4250027
MikeTheFyke <mikefyke@hotmail.com>
2021-03-23 11:42:54 -0400

    Creating array of fly objects

--- client/public/logic/flyLogic.js
+++ client/public/logic/flyLogic.js
@@ -4,17 +4,31 @@ var windowWidth = window.innerWidth;
 var randomYValue;
 var randomY;
 
+var randomXValue;
+var randomX;
+
+var flys = [];
+
 createElement = () => {
     for (var i = 1; i <= 5; i ++){
         randomYValue = Math.floor(Math.random() * (windowHeight - 10)) + 10;
         randomY = randomYValue + "px";
+        randomXValue = Math.floor(Math.random() * (windowWidth - 10)) + 10;
+        randomX = randomXValue + "px";
         var newFly = new Image (50,50);
         newFly.setAttribute("img", "fly"[i]);
         newFly.src = '/public/images/fly.png';
         newFly.style.position = 'absolute';
-        newFly.style.left = '10px';
+        newFly.style.left = randomX;
         newFly.style.top = randomY;
         document.getElementById("flyViewContainer").appendChild(newFly);
+
+        flys.push({
+            id: i,
+            x: randomX,
+            y: randomY
+        })
+        
         randomY = "";
     }
 }
