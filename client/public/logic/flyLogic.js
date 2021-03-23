5386f26fc9b329d15cbb9bfabba3c1b86962f6a7
MikeTheFyke <mikefyke@hotmail.com>
2021-03-23 11:36:09 -0400

    Grabbing window width and height to create random y placement for new flys

--- client/public/logic/flyLogic.js
+++ client/public/logic/flyLogic.js
@@ -1,9 +1,21 @@
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+var randomYValue;
+var randomY;
+
 createElement = () => {
     for (var i = 1; i <= 5; i ++){
+        randomYValue = Math.floor(Math.random() * (windowHeight - 10)) + 10;
+        randomY = randomYValue + "px";
         var newFly = new Image (50,50);
         newFly.setAttribute("img", "fly"[i]);
         newFly.src = '/public/images/fly.png';
+        newFly.style.position = 'absolute';
+        newFly.style.left = '10px';
+        newFly.style.top = randomY;
         document.getElementById("flyViewContainer").appendChild(newFly);
+        randomY = "";
     }
 }
 
