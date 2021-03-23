420e561a8fea796cce0dde686567b4134e15c9f7
MikeTheFyke <mikefyke@hotmail.com>
2021-03-23 11:27:45 -0400

    Added create fly function

--- client/public/logic/flyLogic.js
+++ client/public/logic/flyLogic.js
@@ -1,5 +1,14 @@
-
+createElement = () => {
+    for (var i = 1; i <= 5; i ++){
+        var newFly = new Image (50,50);
+        newFly.setAttribute("img", "fly"[i]);
+        newFly.src = '/public/images/fly.png';
+        document.getElementById("flyViewContainer").appendChild(newFly);
+    }
+}
 
 TweenMax.to('#flyImage', 0, { x: "-100vw" })
 
 TweenMax.to('#flyImage', 4, { x: 0, delay: 1 })
+
+createElement()
