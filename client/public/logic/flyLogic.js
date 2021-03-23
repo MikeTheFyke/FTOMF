514f0f601aa806f403d536643a6c5ceffe47d59d
MikeTheFyke <mikefyke@hotmail.com>
2021-03-23 15:20:53 -0400

    Starting shadows for flys

--- client/public/logic/flyLogic.js
+++ client/public/logic/flyLogic.js
@@ -14,9 +14,10 @@ var randomSpeed;
 var randomDelay;
 var randomScale;
 
-var numberOfFlys = Math.floor(Math.random() * (100 - 20 + 1) + 20);
+var numberOfFlys = Math.floor(Math.random() * (200 - 50 + 1) + 50);
 
 var flys = [];
+var shadows = [];
 
 createElements = () => {
     console.log("NumberOfFlys : " + numberOfFlys)
@@ -25,18 +26,36 @@ createElements = () => {
         randomY = randomYValue + "px";
         randomXValue = Math.floor(Math.random() * (windowWidth - 10)) + 10;
         randomX = randomXValue + "px";
-        var newFly = new Image (50,50);
-        newFly.setAttribute("id", "fly"+ [i]);
-        newFly.src = '/public/images/fly.png';
-        newFly.style.position = 'absolute';
-        newFly.style.left = randomX;
-        newFly.style.top = randomY;
-        document.getElementById("flyViewContainer").appendChild(newFly);
-            flys.push({
-                id: i,
-                x: randomXValue,
-                y: randomYValue
-            })
+
+            var newFly = new Image (50,50);
+            newFly.setAttribute("id", "fly"+ [i]);
+            newFly.src = '/public/images/fly.png';
+            newFly.style.position = 'absolute';
+            newFly.style.left = randomX;
+            newFly.style.top = randomY;
+
+            var newShadow = document.createElement('div');
+            newShadow.setAttribute("id", "shadow"+ [i]);
+            newShadow.style.background = '#000000';
+            newShadow.style.position = 'absolute';
+            newShadow.style.width = '30px';
+            newShadow.style.height = '30px';
+            newShadow.style.borderRadius = '20px';
+            newShadow.style.left = randomX;
+            newShadow.style.top = randomY;
+
+                document.getElementById("flyViewContainer").appendChild(newShadow);
+                document.getElementById("flyViewContainer").appendChild(newFly);
+                    flys.push({
+                        id: i,
+                        x: randomXValue,
+                        y: randomYValue
+                    })
+                    shadows.push({
+                        id: i,
+                        x: randomXValue,
+                        y: randomYValue
+                    })
         randomY = "";
     }
     setElements()
@@ -44,19 +63,19 @@ createElements = () => {
 
 setElements = () => {
     for (var i = 1; i <= numberOfFlys; i ++){
-        randomScale = (Math.floor(Math.random() * (20 - 10 + 1) + 10)) / 10;
+        randomScale = (Math.floor(Math.random() * (90 - 40 + 1) + 40)) / 10;
         console.log(randomScale)
         if ( flys[i - 1].x <= windowWidthHalf && flys[i - 1].y <= windowHeightHalf ){
-            TweenMax.to('#fly'+ i, 0, { x: "-100vw", y: "-100vw" })    
+            TweenMax.to('#fly'+ i, 0, { x: "-100vw", y: "-100vw", scaleX: randomScale, scaleY: randomScale })    
         }
         else if ( flys[i - 1].x <= windowWidthHalf && flys[i - 1].y > windowHeightHalf ){
-            TweenMax.to('#fly'+ i, 0, { x: "-100vw", y: "100vw", rotation: -90 })    
+            TweenMax.to('#fly'+ i, 0, { x: "-100vw", y: "100vw", rotation: -90, scaleX: randomScale, scaleY: randomScale })    
         }
         else if ( flys[i - 1].x > windowWidthHalf && flys[i - 1].y <= windowHeightHalf){
-            TweenMax.to('#fly'+ i, 0, { x: "100vw", y: "-100vw", rotation: 90 })    
+            TweenMax.to('#fly'+ i, 0, { x: "100vw", y: "-100vw", rotation: 90, scaleX: randomScale, scaleY: randomScale })    
         }
         else if ( flys[i - 1].x > windowWidthHalf && flys[i - 1].y > windowHeightHalf){
-            TweenMax.to('#fly'+ i, 0, { x: "100vw", y: "100vw", rotation: 180 })    
+            TweenMax.to('#fly'+ i, 0, { x: "100vw", y: "100vw", rotation: 180, scaleX: randomScale, scaleY: randomScale })    
         }
     }
     moveElements()
@@ -66,8 +85,8 @@ moveElements = () => {
     for (var i = 1; i <= numberOfFlys; i ++){
         randomSpeed = Math.floor(Math.random() * 4) + 1;
         randomDelay = Math.floor(Math.random() * 4) + 1;
-        TweenMax.to('#fly'+ i, randomSpeed, { x: 0, y: 0, delay: randomDelay })    
-    }
+        TweenMax.to('#fly'+ i, randomSpeed, { x: 0, y: 0, delay: randomDelay, scaleX : 1, scaleY: 1 })    
+    }  
 }
 
 createElements()
