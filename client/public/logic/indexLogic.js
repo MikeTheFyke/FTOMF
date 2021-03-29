490f005eebbed8fb0853bc63dbf2e05a6021a13c
MikeTheFyke <mikefyke@hotmail.com>
2021-03-28 22:04:12 -0400

    added walk link logic in header logic

--- client/public/logic/indexLogic.js
+++ client/public/logic/indexLogic.js
@@ -91,3 +91,22 @@ document.getElementById('header-element4').addEventListener('mouseout', function
     TweenMax.to('#header-element4', 1.5, { y:0, scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
     TweenMax.to('#foot-shadow4', 1.5, { scaleY: .5, scaleX: 1, ease: Elastic.easeOut.config(1, 0.3) } );
 });
+
+/// header element 5
+
+TweenMax.to('#foot-shadow5', 0, {scaleY: 0, scaleX: 0})
+TweenMax.to('#foot-shadow5', 1.05, {scaleY: 0.5, scaleX: 1, delay: 1})
+TweenMax.to('#foot-shadow5', 0.5, {scaleY: 0.25, scaleX: 0.75, delay: 2.05})
+TweenMax.to('#foot-shadow5', 0.5, {scaleY: 0.5, scaleX: 1, delay: 2.55})
+TweenMax.to('#foot-shadow5', 0.25, {scaleY: 0.25, scaleX: 0.75, delay: 3.05})
+TweenMax.to('#foot-shadow5', 0.20, {scaleY: 0.5, scaleX: 1, delay: 3.30})
+
+document.getElementById('header-element5').addEventListener('mouseover', function(){
+    TweenMax.to('#header-element5', 1.5, { y: -25, scale: 1.5, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#foot-shadow5', 1.5, { scaleY: .75, scaleX: 1.25, ease: Elastic.easeOut.config(1, 0.3) } );
+});
+
+document.getElementById('header-element5').addEventListener('mouseout', function(){
+    TweenMax.to('#header-element5', 1.5, { y:0, scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#foot-shadow5', 1.5, { scaleY: .5, scaleX: 1, ease: Elastic.easeOut.config(1, 0.3) } );
+});
