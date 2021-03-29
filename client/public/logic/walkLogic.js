7751d0de756f5c269ef383d9fe9a82942b16da91
MikeTheFyke <mikefyke@hotmail.com>
2021-03-28 22:29:47 -0400

    Adding sprite up and right logic

--- client/public/logic/walkLogic.js
+++ client/public/logic/walkLogic.js
@@ -1,14 +1,15 @@
 // Sprite Intro Logic
 TweenMax.to("#sprite", 0, { y: "-100vh" })
-TweenMax.to("#sprite", 3, { y: 0, delay: 1 } );
+TweenMax.to("#sprite", 3, { y: 0, delay: 1 })
 
-document.addEventListener("keydown", spriteMove, false);
+document.addEventListener("keydown", spriteMove, false)
 
 function spriteMove (e) {
-    var keyCode = e.keyCode;
-      if(keyCode==13) {
-      alert("You hit the enter key.");
-      } else {
-      alert("Oh no you didn't.");
+    var keyCode = e.keyCode
+      if(keyCode==38) {
+        TweenMax.to("#sprite", 2, { y: "-20px" })
+        TweenMax.to("#sprite", 1, { y: 0, delay: 1.5 })
+      } else if (keycode==39){
+        TweenMax.to("#sprite", 1, { y: "10px" })
       }
 }
