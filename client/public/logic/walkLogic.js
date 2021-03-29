98d7e6c16c1d7f47e06391c715e8661ff7f02a16
MikeTheFyke <mikefyke@hotmail.com>
2021-03-28 22:48:31 -0400

    Created sprite object with x and y

--- client/public/logic/walkLogic.js
+++ client/public/logic/walkLogic.js
@@ -4,24 +4,31 @@ TweenMax.to("#sprite", 3, { y: 0, delay: 1 })
 
 document.addEventListener("keydown", spriteMove, false)
 
+var Sprite = {
+    x: 0,
+    y: 0
+}
+
 function spriteMove (e) {
     var keyCode = e.keyCode
       if(keyCode==38) {         // Jump Up
         TweenMax.to("#sprite", 2, { y: "-20px" })
         TweenMax.to("#sprite", 1, { y: 0, delay: 1.5 })
       } else if (keyCode==39){  // move Right
-        TweenMax.to("#sprite", 1, { x: "10px" })
+        Sprite.x += 10 
+        TweenMax.to("#sprite", 1, { x: Sprite.x })
       } else if (keyCode==37){  // Move Left
-        TweenMax.to("#sprite", 1, { x: "-10px" })
+        Sprite.x -= 10 
+        TweenMax.to("#sprite", 1, { x: Sprite.x })
       } else if (keyCode==40){  // Bend Down
         TweenMax.to("#sprite", 1, { scaleY: 0.5, transformOrigin: "center bottom" })
         TweenMax.to("#sprite", 1, { scaleY: 1, transformOrigin: "center bottom", delay: 1 })
       } else if (keyCode==39 && kefyCode==39){  // Jump Up and Right
         TweenMax.to("#sprite", 1, { y: "-20px", x: "10px" })
-        TweenMax.to("#sprite", 1, { y: 0, x: "20px", delay: 0.75 })
+        TweenMax.to("#sprite", 0.5, { y: 0, x: "20px", delay: 0.75 })
       } else if (keyCode==39 && kefyCode==37){  // Jump Up and Left
         TweenMax.to("#sprite", 1, { y: "-20px", x: "-10px" })
-        TweenMax.to("#sprite", 1, { y: 0, x: "-20px", delay: 0.75 })
+        TweenMax.to("#sprite", 0.5, { y: 0, x: "-20px", delay: 0.75 })
       }
       
 }
