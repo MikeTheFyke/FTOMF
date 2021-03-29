a19f06c583584c7934b1e883c7385d05e3f78af0
MikeTheFyke <mikefyke@hotmail.com>
2021-03-28 22:36:40 -0400

    Sprite left right jump and squash logic

--- client/public/logic/walkLogic.js
+++ client/public/logic/walkLogic.js
@@ -9,7 +9,12 @@ function spriteMove (e) {
       if(keyCode==38) {
         TweenMax.to("#sprite", 2, { y: "-20px" })
         TweenMax.to("#sprite", 1, { y: 0, delay: 1.5 })
-      } else if (keycode==39){
-        TweenMax.to("#sprite", 1, { y: "10px" })
+      } else if (keyCode==39){
+        TweenMax.to("#sprite", 1, { x: "10px" })
+      } else if (keyCode==37){
+        TweenMax.to("#sprite", 1, { x: "-10px" })
+      } else if (keyCode==40){
+        TweenMax.to("#sprite", 1, { scaleY: 0.5, transformOrigin: "center bottom" })
+        TweenMax.to("#sprite", 1, { scaleY: 1, transformOrigin: "center bottom", delay: 1 })
       }
 }
